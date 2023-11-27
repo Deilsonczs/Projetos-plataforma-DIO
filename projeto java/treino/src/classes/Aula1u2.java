aea1123612aa8820ca3a26436bf64d84f782b370
Deilson Araujo <deilsonaraujo05197@gmail.com>
2023-11-27 12:57:37 -0300

    estudo poo

---
+++ projeto java/treino/src/classes/Aula1u2.java
@@ -0,0 +1,10 @@
+package classes;
+
+public class Aula1u2 {
+    public static void main(String[] args) {
+      System.out.println("olá mundo");
+} 
+    }
+
+
+    
---
+++ projeto java/treino/src/classes/Caneta.java
@@ -0,0 +1,24 @@
+package classes;
+
+public class Caneta {
+    String modelo;
+    public String cor;
+    public float ponta;
+    int carga;
+    public boolean tampada;
+    public void status(){
+        System.out.println("Uma caneta" + this.cor);
+    }
+    void rabiscar(){
+
+    }
+
+    void tampar(){
+
+    }
+
+    void destampar (){
+
+    }
+
+}
